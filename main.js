c4ceefb5457e2523e0cb6209e7ca2185291b91d9
Omar-Abuissa <omargithub2009@gmail.com>
2023-05-19 13:14:54 +0300

    The Whole Project but without _ 1- mega menu 2- the comments in the code 3- the titles of the page 4- the styling 5- arangment of the code

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 // Cannot Write Commands Because I dont know js :(
-const images = ['imgs/team-01.png ' , 'imgs/team-02.png', 'imgs/team-03.png' , 'imgs/team-04.png' , 'imgs/team-05.png'];
+const images = ['imgs/team-01.png', 'imgs/team-04.png', 'imgs/team-02.png', 'imgs/team-07.png', 'imgs/team-06.png', 'imgs/team-05.png', 'imgs/team-03.png'];
 let index = 0;
 const imgElement = document.querySelector('#image');
 
 function change() {
-    imgElement.src = images[index];
-    index > 3 ? index = 0 : index++;
+  imgElement.src = images[index];
+  index > 5 ? index = 0 : index++;
 }
-  
-  window.onload = function() {
-    setTimeout(() => {
-      change();
-      setInterval(() => {
-        if (imgElement.complete) {
-          change();
-        } else {
-          index > 3 ? index = 0 : index++;
-          imgElement.src = images[index];
-        }
-      }, 2000);
-    }, 1000);
+
+window.onload = function() {
+  setTimeout(() => {
+    change();
+    setInterval(() => {
+      if (imgElement.complete) {
+        change();
+      } else {
+        index > 5 ? index = 0 : index++;
+        imgElement.src = images[index];
+      }
+    }, 2000);
+  }, 1000);
 };
